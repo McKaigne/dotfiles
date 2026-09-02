abf10f86e014d5284e0cbb6ca53045f680052955
Pollux <pollux@castor.local>
2026-09-02 21:41:09 +0800

    chore: clean up duplicate configs and legacy root files

--- modules/features/emacs.nix
+++ modules/features/emacs.nix
@@ -1,21 +1,18 @@
 { self, inputs, ... }: {
-  # 1. NixOS System Module (hooks the wrapped package into your OS)
   flake.nixosModules.emacs = { pkgs, ... }: {
     environment.systemPackages = [
       self.packages.${pkgs.stdenv.hostPlatform.system}.myEmacs
     ];
   };
 
-  # 2. Per-System Package Wrapper (Allows running with `nix run`)
   perSystem = { pkgs, lib, ... }:
     let
-      # Tools required by Doom Emacs & Language Servers
       doomRuntimeDeps = with pkgs; [
         git
         ripgrep
         fd
         clang
-        clang-tools # clangd for C++ LSP
+        clang-tools
         cmake
         gnumake
         nixfmt
@@ -24,18 +21,13 @@
         direnv
       ];
 
-      # Store path of your Doom config folder
-      doomConfigDir = ./doom;
-
-      # Wrapped Emacs binary with dependencies & config baked in
       myEmacs = pkgs.symlinkJoin {
         name = "my-doom-emacs";
         paths = [ pkgs.emacs-pgtk ];
         buildInputs = [ pkgs.makeWrapper ];
         postBuild = ''
           wrapProgram $out/bin/emacs \
-            --prefix PATH : ${lib.makeBinPath doomRuntimeDeps} \
-            --set-default DOOMDIR "${doomConfigDir}"
+            --prefix PATH : ${lib.makeBinPath doomRuntimeDeps}
 
           wrapProgram $out/bin/emacsclient \
             --prefix PATH : ${lib.makeBinPath doomRuntimeDeps}
@@ -43,10 +35,7 @@
       };
     in
     {
-      # Exported package: `nix run .#myEmacs`
       packages.myEmacs = myEmacs;
-
-      # Default package & app: `nix run`
       packages.default = myEmacs;
       apps.default = {
         type = "app";
