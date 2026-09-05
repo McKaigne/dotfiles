37a1ce6b25e0b4a66dea6bb1f8ec0a5932a713ab
McKaigne <abellorchristian2007@gmail.com>
2026-09-05 09:38:44 +0800

    feat(home-manager): manage fuzzel.ini and cava config declaratively

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -10,6 +10,8 @@
       home.stateVersion = "25.05";
 
       xdg.configFile."starship/starship.toml".source = ./nushell/starship.toml;
+      xdg.configFile."fuzzel/fuzzel.ini".source = ./fuzzel/fuzzel.ini;
+      xdg.configFile."cava/config".source = ./cava/config;
 
       # Per-app xdg.configFile entries get added here as we migrate
       # each ~/.config/<app> dir.
