5c225c07e063965066d50d5a9c7e3c15e6356e19
McKaigne <abellorchristian2007@gmail.com>
2026-09-06 12:09:40 +0800

    refactor(home-manager): switch from auto-discovery to explicit xdg.configFile entries

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -6,30 +6,33 @@
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
 
-    home-manager.users.pollux = { lib, config, ... }:
+    home-manager.users.pollux = { config, ... }:
       let
-        dotfilesRootPath = ../../dotfiles;
-        dotfilesRootStr = "/etc/nixos/dotfiles";
-
-        walk = rel:
-          let
-            entries = builtins.readDir (dotfilesRootPath + (if rel == "" then "" else "/${rel}"));
-            go = name: type:
-              let relPath = if rel == "" then name else "${rel}/${name}"; in
-              if type == "directory" then walk relPath else [ relPath ];
-          in
-          lib.concatLists (lib.mapAttrsToList go entries);
-
-        files = walk "";
-
-        toEntry = relPath: {
-          name = relPath;
-          value.source = config.lib.file.mkOutOfStoreSymlink "${dotfilesRootStr}/${relPath}";
-        };
+        link = path: config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/${path}";
       in
       {
         home.stateVersion = "25.05";
-        xdg.configFile = builtins.listToAttrs (map toEntry files);
+
+        xdg.configFile = {
+          "niri/config.kdl".source        = link "niri/config.kdl";
+          "nushell/config.nu".source      = link "nushell/config.nu";
+          "nushell/env.nu".source         = link "nushell/env.nu";
+          "starship/starship.toml".source = link "starship/starship.toml";
+          "gtk-3.0/gtk.css".source        = link "gtk-3.0/gtk.css";
+          "gtk-4.0/gtk.css".source        = link "gtk-4.0/gtk.css";
+          "cava/config".source            = link "cava/config";
+          "ghostty/config".source         = link "ghostty/config";
+          "fuzzel/fuzzel.ini".source      = link "fuzzel/fuzzel.ini";
+
+          "doom/config.el".source    = link "doom/config.el";
+          "doom/init.el".source      = link "doom/init.el";
+          "doom/custom.el".source    = link "doom/custom.el";
+          "doom/packages.el".source  = link "doom/packages.el";
+          "doom/carabao.svg".source  = link "doom/carabao.svg";
+          "doom/themes/doom-nano-dark-theme.el".source  = link "doom/themes/doom-nano-dark-theme.el";
+          "doom/themes/doom-nano-light-theme.el".source = link "doom/themes/doom-nano-light-theme.el";
+          "doom/themes/noctalia-theme.el".source        = link "doom/themes/noctalia-theme.el";
+        };
       };
   };
 }
