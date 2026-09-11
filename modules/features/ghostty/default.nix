3e8933214b5f38ef7143d1f538567bd79cb2b5eb
McKaigne <abellorchristian2007@gmail.com>
2026-09-12 01:12:15 +0800

    chore: clean up

--- modules/features/ghostty/default.nix
+++ modules/features/ghostty/default.nix
@@ -16,9 +16,6 @@ in
         font-size = 14
         theme = noctalia
         window-decoration = false
-        window-padding-x = 12
-        window-padding-y = 10
-        window-padding-balance = true
         cursor-style = block
         cursor-style-blink = false
         cursor-color = "#ffffff"
